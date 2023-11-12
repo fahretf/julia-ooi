3365ea8b78f4c898246c2cb11fac29290b5c100a
isthisfahret <73430835+isthisfahret@users.noreply.github.com>
2023-11-12 21:49:08 +0100

    LV 3-4.

--- lv3-4.jl
+++ lv3-4.jl
@@ -19,26 +19,33 @@ end
 
 function rijesi_simplex(A, b, c) 
         jedinicna_matrica = I(size(A, 1))
+        
+
         tabela = [A jedinicna_matrica]
-        append!(c, [0, 0])
+
+        append!(c, fill(0, size(jedinicna_matrica, 1)))
+
         c = reshape(c, 1, length(c))
         tabela = [tabela; c]
+
         push!(b, 0)
         tabela = [b tabela]
-        println("Prikaz tabele: ")
-        display(tabela)
-        println("")
-        base = [3, 4] # indeksi baza
+        base = Vector{Int}() # mozda greska jer ovako deklarisem vektor
+
+        for i = 1:size(A,2)
+            push!(base, size(A,2)+i)
+        end
+
+
 
         last_row = tabela[end, 2:end]
         max_element = maximum(last_row)
         max_index = argmax(last_row)
-        max_index += 1 #mozda ne treba ovde +=1?
-    
-        index_vodece_kolone =0
+        max_index += 1 
+
         while max_element > 0
             q=max_index;
-            boolean = provjeri_vrijednosti_kolone(tabela[:, q])
+            boolean = provjeri_vrijednosti_kolone(tabela[1:end-1, q])
             if boolean == false 
                 println("Rjesenje je neograniceno")
                 return;
@@ -50,76 +57,77 @@ function rijesi_simplex(A, b, c)
                 vektor_t[i] = tabela[i, 1] / tabela[i,q]
             end 
             
-
-            
             p = 0
             minimalna_vrijednost = minimum(vektor_t)
             p=argmin(vektor_t) # vodeci red
 
+            base[p] = q-1
 
-            base[p] = q
+
+
             pivot = tabela[p,q]
-           # println("Pivot element je: $pivot")
-
-           # temp1 = readline()
-           # println("Vodeci red je: $p")
-           # temp2=readline()
-           # println("Tabela nakon sto podijelimo red s pivot elementom:  ")
-            tabela[p, :] = tabela[p, :] ./ pivot
-            #display(tabela)
-
-           # temp3 = readline()
-
           
 
-            
-            for i = 1:size(tabela, 1)
+          
+            B=deepcopy(tabela)
+
+            for i = 1:size(B, 1)
                 if i == p    
                     continue
                 end    
-                    for j = 1:size(tabela, 2)
-                        tabela[i,j] = tabela[i,j] - (tabela[i,q] * tabela[p,j] / pivot)
+                    for j = 1:size(B, 2)
+                        k = tabela[i,j]
+                        tabela[i,j] = B[i,j] - (B[i,q] * B[p,j]) / pivot
                     end
             end
 
 
-           # println("Tabela nakon sto izvrsimo one divje petlje: ")
 
-           # display(tabela)
+            tabela[p, :] = tabela[p, :] ./ pivot
 
-            #temp3 = readline()
+          
+
             last_row = tabela[end, 2:end]
             max_element = maximum(last_row)
             max_index = argmax(last_row)
-            max_index += 1 #mozda ne treba ovde +=1?
-            println("Pazi sad: ")
-            display(tabela)
+            max_index += 1
+
 
 
         end
 
-        funkcija_cilja = tabela[3, 1]
-        promenljive=zeros(4) 
+        promenljive=zeros(size(tabela, 2)-1)
+        funkcija_cilja = tabela[end, 1]
 
-       # for i = 1:5
-       #     promenljive[i] = tabela[3,i]
-       # end
 
-       # for i in base 
-       #     promenljive[i] = 0
-       # end
+        kontrolna_promjenljiva=1
+        for i in base
+            promenljive[i] = tabela[kontrolna_promjenljiva, 1]
+            kontrolna_promjenljiva+=1
+        end    
 
-       println("###### KRajnja tabela: ")
+        println("Iz nacina kako je formiran vektor promjenljivih, ne-nulte promjenljive su automatski bazne promjenljive, dok su one promjenljive koje imaju vrijednost nula ustvari promjenljive koje nisu u bazi.")
+        display(promenljive)
 
-       display(tabela)
+        println("Vrijednost funkcije cilja je: ")
+        display(tabela[end, 1])
 
-        println("OK")
+
 end
 
 
+# A=[6.0 9.0; 2.0 1.0]
+# b=[100.0, 20.0]
+# c=[2000.0, 3000.0]
+
+# A=[1.0 0.0; 1.0 -1.0]    
+# b=[7.0, 8.0]
+# c=[5.0, 4.0]
+
 A=[0.5 0.3; 0.1 0.2]
-b=[150, 60]
-c=[3, 1]
+b=[150.0, 60.0]
+c=[3.0, 1.0]
+
 
 
 rijesi_simplex(A, b, c)
